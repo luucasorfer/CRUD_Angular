63f4f877c1f723b5be745e7d1ad853a3a3a26f43
luucasorfer <lucas.fernandes.94.lf@gmail.com>
2022-03-29 19:49:08 -0300

    Componente Início (home) - criação do componente, inserção no header e importação para app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component'
-
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    NavComponent,
     FooterComponent,
-    NavComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +28,7 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatCardModule,
     MatListModule
   ],
   providers: [],
